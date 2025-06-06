5de5cd04dc7b5f6733b7bbc2f3bbb55aa0c1e2d9
vinodh241 <a.vinodh9072@gmail.com>
2025-06-06 12:07:39 +0530

    shell-roboshop-2

--- roboshop.sh
+++ roboshop.sh
@@ -6,7 +6,10 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipp
 ZONE_ID="Z07082243VUB84KU714AG"
 DOMAIN_NAME= "vinodh.site"
 
-for instances in ${INSTANCES[@]}
+#for instances in ${INSTANCES[@]}
+
+for instances in $@
+
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-052fc2c712d4ff706 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
