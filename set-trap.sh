9f523211e7cf276d571c5a4ac1a19340d9f0915a
vinodh241 <a.vinodh9072@gmail.com>
2025-06-22 22:55:59 +0530

    shell-set and trap-functions

---
+++ set-trap.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+
+set -e 
+ FAILURE(){
+    echo "failed at : $1 $2 "
+
+ }
+
+trap 'FAILURE "${LINENO}" "$(BASH_COMMAND)"' ERR
+
+
+START_TIME=$(date +%s)  # here declearing start time 
+USERID=$( id -u )
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+
+mkdir -p $LOGS_FOLDER
+echo "script started execution: $(date)"
+
+if [ $USERID -ne 0 ]
+then
+   echo -e "$R ERROR:: Please run this script with root access $N " | tee -a &LOG_FILE
+   exit 1
+else
+   echo -e "$G youre running with root access $N " | tee -a $LOG_FILE
+   fi
+
+dnf module disable nodejs -y &>>$LOG_FILE
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+dnf install nodejsyyyyeye -y &>>$LOG_FILE
+
+
+id roboshop
+if [ $? -ne 0 ]
+then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+    VALIDATE $? "Creating roboshop system user"
+else
+    echo -e "System user roboshop already created ... $Y SKIPPING $N"
+fi
+
+mkdir -p /app 
+curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip &>>$LOG_FILE
+
+rm -rf /app/*
+cd /app 
+unzip /tmp/user.zip &>>$LOG_FILE
+
+
+npm install &>>$LOG_FILE
+
+
+cp $SCRIPT_DIR/user.service /etc/systemd/system/user.service
+
+systemctl daemon-reload &>>$LOG_FILE
+systemctl enable user &>>$LOG_FILE
+systemctl start user
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+
+
+echo -e " Script execution completed successfully $Y total time taken : $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
