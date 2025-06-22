81cad65c99f25a1a3adcad692fe5b7051c4d17be
vinodh241 <a.vinodh9072@gmail.com>
2025-06-22 17:29:45 +0530

    shell-roboshop-2

---
+++ common.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+START_TIME=$( date +%s )
+USERID=$( id - u )
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/roboshop-2-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE=$LOGS_FOLDER/$SCRIPT_NAME.log
+SCRIPT_DIR=$PWD
+
+check_root(){
+      if [ $USERID -ne 0 ]
+      then
+         echo -e "$R ERROR: : $Y Please run this script with root access $N"
+         exit 1
+      else
+         echo -e "$G You are running with root access $N"
+      fi
+}
+
+# VALIDATE functions takes input as exit  status, what command they tried to install 
+    VALIDATE(){
+        if [ $1 -eq 0 ]
+        then
+           echo -e "$G Installing $2 is ..... SUCCESS $N"
+        else
+           echo -e "$R Installing $2 is ..... FAILURE $N"
+           exit 1 
+        fi           
+}
+
+print_time(){
+      END_TIME=$(date +%s)
+      TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+
+      echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
+}
+
+
---
+++ mongodb.sh
@@ -0,0 +1,26 @@
+
+source ./common.sh
+app_name=monogodb
+
+check_root
+
+cp mongodb.repo  /etc/yum.repos.d/mongodb.repo
+VALIDATE $? "copying mongodb.repo"
+
+dnf install mongodb-org -y  &>>$LOG_FILE
+VALIDATE $? "installing mongodb"
+
+systemctl enable mongod &>>$LOG_FILE
+VALIDATE $? "enabling mongodb"
+
+systemctl start mongod 
+VALIDATE $? "starting mongodb"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "Editing mongodb conf file for remote connections"
+
+systemctl restart mongod &>>$LOG_FILE
+VALIDATE $? "Restrating MOngoDB"
+
+print_time
+
