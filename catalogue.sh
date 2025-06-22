cbc50352160e3f33d3fb3ec2afcf8fc37d95ab56
vinodh241 <a.vinodh9072@gmail.com>
2025-06-22 18:25:44 +0530

    shell-roboshop-2-catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 source ./common.sh
+
 app_name=catalogue
+
 check_root
+
 app_setup
+
 nodejs_setup
 
 systemd_setup
 
-cp mongodb.repo /etc/yum.repos.d/mongo.repo 
+cp $SCRIPT_DIR/mongodb.repo /etc/yum.repos.d/mongo.repo 
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Installing MongoDB Client"
 
