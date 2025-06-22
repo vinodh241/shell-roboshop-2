e5c796c2618f6f780fd5e6b26c2b1028e8944586
vinodh241 <a.vinodh9072@gmail.com>
2025-06-22 21:44:34 +0530

    shell-roboshop-2-mysql

---
+++ mysql.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+source./common.sh
+app_name=mysql
+check_root
+
+echo "Please enter thw mysql root password"
+read -s MYSQL_ROOT_PASSWORD
+
+
+
+dnf install mysql-server -y &>>$LOG_FILE
+VALIDATE $? "installing MySql server"
+
+systemctl enable mysqld
+VALIDATE $? "enablling MySql server"
+
+systemctl start mysqld  
+VALIDATE $? "starting MySql Server"
+
+
+mysql_secure_installation --set-root-pass $MYSQL_ROOT_PASSWORD
+VALIDATE $? " setuping root password to login mysql server"
+
+print_time
